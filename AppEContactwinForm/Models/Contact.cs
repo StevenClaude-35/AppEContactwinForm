c8547c1e51a485d8186b0d7b6f5c0304ea40632c
claudy <nabab35000@gmail.com>
2020-08-08 03:15:21 +0200

    Ajoutez des fichiers projet.

---
+++ AppEContactwinForm/Models/Contact.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppEContactwinForm.Models
+{
+   public  class Contact
+    {
+        public int Id { get; set; }
+        public string Nomcomplet { get; set; }
+        public DateTime DateNaiss { get; set; }
+        public string Email { get; set; }
+        public string Telephone { get; set; }
+        public string Genre { get; set; }
+        public string UrlPhoto { get; set; }
+
+        public Contact()
+        {
+
+        }
+
+        public Contact(string nomcomplet, DateTime dateNaiss, string email, string telephone, string genre, string urlPhoto)
+        {
+            Nomcomplet = nomcomplet;
+            DateNaiss = dateNaiss;
+            Email = email;
+            Telephone = telephone;
+            Genre = genre;
+            UrlPhoto = urlPhoto;
+        }
+    }
+}
